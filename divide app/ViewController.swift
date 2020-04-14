d8132da860e4aee70df73667257119db7109f51d
Zarnold Aziz <zarnoldaziz@gmail.com>
2020-04-14 13:05:59 +0700

    Initial Commit

---
+++ divide app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  divide app
+//
+//  Created by zarnold aziz on 14/04/20.
+//  Copyright © 2020 zarnold aziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
